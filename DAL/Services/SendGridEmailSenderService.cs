1b1e75b472a6ed39fad2008036ca60fe9425d3b5
lenepalu <lenepalu@yahoo.fr>
2020-07-08 11:33:34 +0000

    set email service

--- DAL/Services/SendGridEmailSenderService.cs
+++ DAL/Services/SendGridEmailSenderService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
@@ -10,8 +11,11 @@ namespace DAL.Services
 {
     class SendGridEmailSenderService
     {
-        public SendGridEmailSenderService(IOptions<SendGridEmailAuthOptions> optionsAccessor)
+        private readonly IConfiguration _configuration;
+
+        public SendGridEmailSenderService(IOptions<SendGridEmailAuthOptions> optionsAccessor,IConfiguration configuration)
         {
+            _configuration = configuration;
             Options = optionsAccessor.Value;
         }
 
@@ -20,7 +24,8 @@ namespace DAL.Services
         public Task SendEmailAsync(List<string> emails, string subject, string message)
         {
 
-            return Execute(Environment.GetEnvironmentVariable("SENDEMAILDEMO_ENVIRONMENT_SENDGRID_KEY"), subject, message, emails);
+//            return Execute(Environment.GetEnvironmentVariable("SENDEMAILDEMO_ENVIRONMENT_SENDGRID_KEY"), subject, message, emails);
+            return Execute(_configuration["SendGrid:ApiKey"], subject, message, emails);
         }
 
         public Task Execute(string apiKey, string subject, string message, List<string> emails)
@@ -28,7 +33,7 @@ namespace DAL.Services
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
-                From = new EmailAddress("noreply@domain.com", "Bekenty Jean Baptiste"),
+                From = new EmailAddress("noreply@"+ _configuration["SendGrid:Domain"], _configuration["SendGrid:Name"]),
                 Subject = subject,
                 PlainTextContent = message,
                 HtmlContent = message
